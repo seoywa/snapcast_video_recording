41cd3c4372ce68b29bc96d9e0f7665969686df87
seoywa <132980713+seoywa@users.noreply.github.com>
2025-05-13 14:38:55 +0700

    Fix visibility type error by adding VideoFormValues to formData state, finish upload page

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -3,20 +3,52 @@
 import FileInput from "@/components/FileInput";
 import FormField from "@/components/FormField";
 import { MAX_THUMBNAIL_SIZE, MAX_VIDEO_SIZE } from "@/constants";
+import {
+  getThumbnailUploadUrl,
+  getVideoUploadUrl,
+  saveVideoDetails,
+} from "@/lib/actions/video";
 import { useFileInput } from "@/lib/hooks/useFileInput";
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
+const uploadFileToBunny = (
+  file: File,
+  uploadUrl: string,
+  accessKey: string
+): Promise<void> => {
+  return fetch(uploadUrl, {
+    method: "PUT",
+    headers: {
+      "Content-Type": file.type,
+      AccessKey: accessKey,
+    },
+    body: file,
+  }).then((response) => {
+    if (!response.ok) throw new Error("Upload failed");
+  });
+};
 
 const UploadPage = () => {
+  const router = useRouter();
+
+  const [videoDuration, setVideoDuration] = useState(0);
   const [error, setError] = useState<string | null>(null);
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<VideoFormValues>({
     title: "",
     description: "",
+    tags: '',
     visibility: "public",
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
   const video = useFileInput(MAX_VIDEO_SIZE);
   const thumbnail = useFileInput(MAX_THUMBNAIL_SIZE);
+
+  useEffect(() => {
+    if (video.duration !== 0) {
+      setVideoDuration(video.duration);
+    }
+  }, [video.duration]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -31,26 +63,59 @@ const UploadPage = () => {
 
     try {
       if (!video.file || !thumbnail.file) {
-        setError('Please upload video and thumbnail');
-        return
+        setError("Please upload video and thumbnail");
+        return;
       }
 
       if (!formData.title || !formData.description) {
-        setError('Please fill in all the details')
-        return
+        setError("Please fill in all the details");
+        return;
       }
 
       // Upload the video to BUNNY
-      // Upload the thumbnail to database
-      // Attach thumbnail 
-      // Create a new data entry for the video details (urls, data)
+      const {
+        videoId,
+        uploadUrl: videoUploadUrl,
+        accessKey: videoAccessKey,
+      } = await getVideoUploadUrl();
 
+      if (!videoUploadUrl || !videoAccessKey)
+        throw new Error("Failed to get video upload credentials");
+
+      await uploadFileToBunny(video.file, videoUploadUrl, videoAccessKey);
+      
+      // Upload the thumbnail to database
+      const {
+        uploadUrl: thumbnailUploadUrl,
+        accessKey: thumbnailAccessKey,
+        cdnUrl: thumbnailCdnUrl,
+      } = await getThumbnailUploadUrl(videoId);
+
+      if (!thumbnailUploadUrl || !thumbnailCdnUrl || !thumbnailAccessKey)
+        throw new Error("Failed to get thumbnail upload credentials");
+
+      // Attach thumbnail
+      await uploadFileToBunny(
+        thumbnail.file,
+        thumbnailUploadUrl,
+        thumbnailAccessKey
+      );
+
+      // Create a new data entry for the video details (urls, data)
+      await saveVideoDetails({
+        videoId,
+        thumbnailUrl: thumbnailCdnUrl,
+        ...formData,
+        duration: videoDuration,
+      });
+
+      router.push(`/video/${videoId}`);
     } catch (error) {
-      console.log("Error submitting form: ", error)
+      console.log("Error submitting form: ", error);
     } finally {
-      setIsSubmitting(false)
+      setIsSubmitting(false);
     }
-  }
+  };
 
   return (
     <div className="wrapper-md upload-page">
@@ -90,7 +155,7 @@ const UploadPage = () => {
           onReset={video.resetFile}
           type="video"
         />
-        
+
         <FileInput
           id="thumbnail"
           label="Thumbnail"
@@ -115,8 +180,8 @@ const UploadPage = () => {
           ]}
         />
 
-        <button type='submit' disabled={isSubmitting} className="submit-button">
-          {isSubmitting ? 'Uploading' : 'Upload video'}
+        <button type="submit" disabled={isSubmitting} className="submit-button">
+          {isSubmitting ? "Uploading" : "Upload video"}
         </button>
       </form>
     </div>
--- constants/index.ts
+++ constants/index.ts
@@ -2,11 +2,11 @@ export const MAX_VIDEO_SIZE = 500 * 1024 * 1024;
 export const MAX_THUMBNAIL_SIZE = 10 * 1024 * 1024;
 
 export const BUNNY = {
-  STREAM_BASE_URL: "https://video.bunnycdn.com/library",
-  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/snapcast",
-  CDN_URL: "https://snapcast.b-cdn.net",
+  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/my-snapcast-zone",
+  CDN_URL: "https://my-snapcast-zone.b-cdn.net",
+  TRANSCRIPT_URL: "https://vz-b0c3c8ab-113.b-cdn.net",
   EMBED_URL: "https://iframe.mediadelivery.net/embed",
-  TRANSCRIPT_URL: "https://vz-47a08e64-84d.b-cdn.net",
+  STREAM_BASE_URL: "https://video.bunnycdn.com/library",
 };
 
 export const emojis = ["😂", "😍", "👍"];
